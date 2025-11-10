6ea732ef881d0ceddd61e44b80cc614db278083d
Luiggi <luiggiyantas@gmail.com>
2025-11-10 16:44:08 -0500

    style(scss): apply BEM naming convention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { LikeComponent } from './like/like.component';
-import { UserFormComponent } from './user-form/user-form.component';
 import { ContainerModule } from './container/container.module';
+import { LikeComponent } from './like/like.component';
+import { ProductModule } from './product/product.module';
+import { UserFormComponent } from './user-form/user-form.component';
+import { ProductBemModule } from './product-bem/product-bem.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LikeComponent, UserFormComponent, ContainerModule],
+  imports: [LikeComponent, UserFormComponent, ContainerModule, ProductModule, ProductBemModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
